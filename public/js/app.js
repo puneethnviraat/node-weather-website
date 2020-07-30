be91e9103fb3c52472ca7902ccc84dc437b0a592
Puneeth <puneethnviraat@gmail.com>
2020-07-30 13:42:04 +0530

    heroku changed tha api company

--- public/js/app.js
+++ public/js/app.js
@@ -8,13 +8,16 @@ message_one.textContent = '';
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = search.value;
-fetch('https://api.weatherstack.com/current?access_key=36e30cb5c24c7928fe571b1d9f4fd2ad&query='+location+'').then((response) => {
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+location+'&appid=7658e29ee6204da60dcec48c4cd297a5').then((response) => {
     response.json().then((data) => {
-        if (data.error) {
-            message_one.textContent=(data.error.info)  
+        if (data.cod==404) {
+            message_one.textContent=(data.message)  
         } else {
-            message_one.textContent=(data.location.name);
-            message_Two.textContent=(data.current.temperature);
+            message_one.textContent = (data.name);
+            const temp_now = Math.round((data.main.temp - 273.15) * 100) / 100;
+
+
+            message_Two.textContent='The temperature is '+temp_now+' and the weather is like '+data.weather[0].description+'.';
         }
     })
 })
